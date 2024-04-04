dcc24e4fcdac28224dee76d3e57c5618a50883ff
Taylor Shen <shen.taylor@yahoo.com>
2024-04-04 14:19:40 -0700

    Added footer

---
+++ src/components/Footer/index.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Footer = () => {
+    return (
+        <div className="bg-white text-slate">
+            <div className="container justify-left border-t border-slate-400 pb-10">
+                <p>© 2024 ByTeams</p>
+            </div>
+        </div>
+    )
+}
+
+export default Footer;
--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -1,13 +1,14 @@
-import { useState } from 'react'
-import { Dialog } from '@headlessui/react'
-import { Bars3Icon, XMarkIcon, ArrowRightEndOnRectangleIcon } from '@heroicons/react/24/outline'
+import { useState } from 'react';
+import { Dialog } from '@headlessui/react';
+import { Bars3Icon, XMarkIcon, ArrowRightEndOnRectangleIcon } from '@heroicons/react/24/outline';
 //import LogoW from '../assets/images/ByTeams_Logo_W.png'
-import LogoB from '../assets/images/ByTeams_Logo_B.png'
+import LogoB from '../assets/images/ByTeams_Logo_B.png';
+import Footer from '../Footer';
 
 const navigation = [
   { name: 'Product', href: '#' },
   { name: 'Features', href: '#' },
-  { name: 'Marketplace', href: '#' },
+  { name: 'Resources', href: '#' },
   { name: 'Company', href: '#' },
 ]
 
@@ -103,13 +104,13 @@ const Home = () => {
           className="absolute inset-x-0 -top-40 -z-10 transform-gpu overflow-hidden blur-3xl sm:-top-80"
           aria-hidden="true"
         >
-          <div
+          {/* <div
             className="relative left-[calc(50%-11rem)] aspect-[1155/678] w-[36.125rem] -translate-x-1/2 rotate-[30deg] bg-gradient-to-tr from-[#ff80b5] to-[#9089fc] opacity-30 sm:left-[calc(50%-30rem)] sm:w-[72.1875rem]"
             style={{
               clipPath:
                 'polygon(74.1% 44.1%, 100% 61.6%, 97.5% 26.9%, 85.5% 0.1%, 80.7% 2%, 72.5% 32.5%, 60.2% 62.4%, 52.4% 68.1%, 47.5% 58.3%, 45.2% 34.5%, 27.5% 76.7%, 0.1% 64.9%, 17.9% 100%, 27.6% 76.8%, 76.1% 97.7%, 74.1% 44.1%)',
             }}
-          />
+          /> */}
         </div>
         <div className="mx-auto max-w-2xl py-32 sm:py-48 lg:py-56">
           <div className="text-center">
@@ -141,6 +142,7 @@ const Home = () => {
             }}
           />
         </div> */}
+      <Footer/>
       </div>
     </div>
   )
